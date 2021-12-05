e81008f311810be8a943158615799a8845ff245f
AbimaelLemus <bisman.lemus@gmail.com>
2021-12-05 01:20:09 -0600

    0.0.3 - Abimael Perez Lemus - se agrego: permiso de internet, link de consumo a README.md,  dependencias y en MainActivity.kt se deja codigo para verificar probar el consumo

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Hero_es
 
+Consumo
+https://akabab.github.io/superhero-api/api
+
+traer toda la informacion
+/all.json
+
+concatenar "/id.json", para traer informacion en particular
+/id
+/powerstats
+/appearance
+/biography
+/connections
+/work
--- app/src/main/java/com/easyapp/hero_es/MainActivity.kt
+++ app/src/main/java/com/easyapp/hero_es/MainActivity.kt
@@ -2,10 +2,52 @@ package com.easyapp.hero_es
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.android.volley.Request
+import com.easyapp.hero_es.databinding.ActivityMainBinding
+import com.google.gson.Gson
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            verificarConsumo.setOnClickListener {
+                consumoSimple()
+                loader(true)
+            }
+        }
+    }
+
+    fun consumoSimple() {
+
+        val url = "https://akabab.github.io/superhero-api/api/work/1.json"
+
+        val queue = Volley.newRequestQueue(this)
+        val solicitud =
+            StringRequest(Request.Method.GET, url, { response ->
+                try {
+                    //val data = Gson().fromJson(response, model::class.java)
+                    Log.w(TAG, "consumoSimple: $response" )
+                } catch (e: Exception) {
+                    Log.e(TAG, "solicitudHTTPVolley: ${e.stackTrace}")
+                }
+                loader(false)
+            }, {
+
+            })
+
+        queue.add(solicitud)
+    }
+
+    fun loader(mostrar:Boolean){
+        binding.progressBar.visibility = if (mostrar)View.VISIBLE else View.INVISIBLE
     }
 }
